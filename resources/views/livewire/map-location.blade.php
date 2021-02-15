dceca4bdf50f64012b929c0dad0f62251d18eef1
AdiatsaPutra <santikosetyo77@gmail.com>
2021-02-15 13:10:09 +0700

    Add lattitude longtitude form

--- resources/views/livewire/map-location.blade.php
+++ resources/views/livewire/map-location.blade.php
@@ -6,7 +6,22 @@
                     <h4 class="card-title">Form</h4>
                 </div>
                 <div class="card-body">
-                    <p class="card-text">Text</p>
+                    <form>
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <div class="mb-3">
+                                    <label for="lattitude" class="form-label">Lattitude</label>
+                                    <input type="text" class="form-control" id="lattitude">
+                                </div>
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="mb-3">
+                                    <label for="longtitude" class="form-label">Longtitude</label>
+                                    <input type="password" class="form-control" id="longtitude">
+                                </div>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
@@ -27,14 +42,25 @@
 <script>
     const defaultLocation = ['110.32743040296015', '-7.887912013104355'];
 
-    // Initialize MapBox View
+    // Initialize Mapbox View
     mapboxgl.accessToken = '{{ env('MAPBOX_KEY') }}';
     var map = new mapboxgl.Map({
         container: 'map',
         center: defaultLocation,
-        zoom: 13,
-        style: 'mapbox://styles/mapbox/streets-v11'
+        zoom: 20,
     });
+
+    // Styling Mapbox Map
+    const lightMapView = 'light-v10';
+    const darkView = 'light-v10';
+    const outdoorsMapView = 'outdoors-v11';
+    const satelliteView = 'satellite-v9';
+    const defaultMapView = 'streets-v11';
+
+    map.setStyle(`mapbox://styles/mapbox/${satelliteView}`);
+
+    // Add Map Controller
+    map.addControl(new mapboxgl.NavigationControl())
 
     // Get Default Location
     map.on('click', (e) => {
