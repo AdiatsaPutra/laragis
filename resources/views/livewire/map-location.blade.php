e71869939fa301c7440b87da26a98cfbb288e1e6
AdiatsaPutra <santikosetyo77@gmail.com>
2021-02-15 11:24:50 +0700

    Stylize Mapbox View

--- resources/views/livewire/map-location.blade.php
+++ resources/views/livewire/map-location.blade.php
@@ -1,11 +1,47 @@
-<div id='map' style='width: 100%; height: 800px;'></div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-4 mb-5">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title">Form</h4>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">Text</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title">Form</h4>
+                </div>
+                <div class="card-body">
+                    <div wire:ignone id='map' style='width: 100%; height: 80vh;'></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 @push('scripts')
 <script>
-    mapboxgl.accessToken = 'pk.eyJ1IjoiYWRpYXRzYSIsImEiOiJja2w1eWhlOXMxcHdxMnBvZXVkcmhnaXF6In0.kZ56zJwTnSp0r5VH3cIKEg';
+    const defaultLocation = ['110.32743040296015', '-7.887912013104355'];
+
+    // Initialize MapBox View
+    mapboxgl.accessToken = '{{ env('MAPBOX_KEY') }}';
     var map = new mapboxgl.Map({
         container: 'map',
+        center: defaultLocation,
+        zoom: 13,
         style: 'mapbox://styles/mapbox/streets-v11'
+    });
+
+    // Get Default Location
+    map.on('click', (e) => {
+        const longtitude = e.lngLat.lng;
+        const lattitude = e.lngLat.lat;
+
+        console.log(longtitude, lattitude);
     });
 </script>
 @endpush
