ec08e946304b9fa3e7bbb734c7b3034fd29e143f
AdiatsaPutra <santikosetyo77@gmail.com>
2021-03-02 08:13:06 +0700

    Stylize Table Content

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,195 +2,215 @@
 
 @section('content')
 <div class="map-location container-fluid">
-  <div class="row mb-3">
-    <h3 class="font-weight-bold">Masukkan Data Survey Lokasi</h3>
-  </div>
-  <div class="row">
-    <div class="col-md-4 mb-5">
-      <form>
-        <div class="row">
-          <div class="col-sm-6">
-            <div class="mb-1">
-              <label for="lattitude" class="form-label">Lattitude</label>
-              <input wire:model="lat" type="text" class="form-control" placeholder="Masukkan Lattitude" id="lat">
-            </div>
-          </div>
-          <div class="col-sm-6">
-            <div class="mb-1">
-              <label class="form-label">Longtitude</label>
-              <input wire:model="long" type="text" class="form-control" placeholder="Masukkan Longtitude" id="lng">
-            </div>
-          </div>
-          <div class="col-sm-12">
-            <div class="mb-1">
-              <label class="form-label">Nama Lokasi</label>
-              <input type="text" class="form-control" placeholder=" Masukan Nama lokasi">
-            </div>
-          </div>
-          <div class="col-sm-12">
-            <div class="mb-1">
-              <label class="form-label">Kategori</label>
-              <input type="text" class="form-control" placeholder=" Masukan Jenis Lokasi">
-            </div>
-          </div>
-          <div class="col-sm-6">
-            <div class="mb-1">
-              <label class="form-label">RT</label>
-              <input type="text" class="form-control" placeholder="Masukkan RT">
-            </div>
-          </div>
-          <div class="col-sm-6">
-            <div class="mb-1">
-              <label class="form-label">RW</label>
-              <input type="text" class="form-control" placeholder="Masukkan RW">
-            </div>
-          </div>
-          <div class="col-sm-12">
-            <div class="mb-1">
-              <label class="form-label">Kelurahan</label>
-              <input type="text" class="form-control" placeholder=" Masukan Kelurahan">
-            </div>
-          </div>
-          <div class="col-sm-12">
-            <div class="mb-1">
-              <label class="form-label">Kecamatan</label>
-              <input type="text" class="form-control" placeholder=" Masukan Kecamatan">
-            </div>
-          </div>
-          <div class="col-sm-6">
-            <div class="mb-1">
-              <label class="form-label">PIC 1</label>
-              <input type="text" class="form-control" placeholder="Masukkan PIC 1">
-            </div>
-          </div>
-          <div class="col-sm-6">
-            <div class="mb-1">
-              <label class="form-label">Telepon</label>
-              <input type="text" class="form-control" placeholder="Masukan Telepon">
-            </div>
-          </div>
-          <div class="col-sm-6">
-            <div class="mb-1">
-              <label class="form-label">PIC 2</label>
-              <input type="text" class="form-control" placeholder="Masukkan PIC 2">
-            </div>
-          </div>
-          <div class="col-sm-6">
-            <div class="mb-1">
-              <label class="form-label">Telepon</label>
-              <input type="text" class="form-control" placeholder="Masukan Telepon">
-            </div>
-          </div>
-          <div class="col-sm-12">
-            @guest
-            @if (Route::has('login'))
-            @endif
-            @if (Route::has('register'))
-            @endif
-            @else
-            <div class="mb-1">
-              <input type="hidden" class="form-control" value="{{ Auth::user()->name }}">
-            </div>
-            @endguest
-          </div>
-          <div class="col-sm-12">
-            <div class="mb-1">
-              <label class="form-label">Tanggal Disurvey</label>
-              <input type="text" class="form-control" id="tgl">
-            </div>
-          </div>
-          <div class="col-sm-12">
-            <div class="mb-1">
-              <label class="form-label">Foto Lokasi</label>
-              <input type="file" class="form-control" id="foto">
-            </div>
-          </div>
-          <div class="col-sm-12">
-            <button class="btn btn-primary btn-block mt-3" type="button">Submit</button>
-          </div>
+    <div class="row mb-3">
+        <h3 class="font-weight-bold">Masukkan Data Survey Lokasi</h3>
+    </div>
+    <div class="row">
+        <div class="col-md-4 mb-5">
+            <form>
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="mb-1">
+                            <label for="lattitude" class="form-label">Lattitude</label>
+                            <input wire:model="lat" type="text" class="form-control" placeholder="Masukkan Lattitude"
+                                id="lat">
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="mb-1">
+                            <label class="form-label">Longtitude</label>
+                            <input wire:model="long" type="text" class="form-control" placeholder="Masukkan Longtitude"
+                                id="lng">
+                        </div>
+                    </div>
+                    <div class="col-sm-12">
+                        <div class="mb-1">
+                            <label class="form-label">Nama Lokasi</label>
+                            <input type="text" class="form-control" placeholder=" Masukan Nama lokasi">
+                        </div>
+                    </div>
+                    <div class="col-sm-12">
+                        <div class="mb-1">
+                            <label class="form-label">Kategori</label>
+                            <input type="text" class="form-control" placeholder=" Masukan Jenis Lokasi">
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="mb-1">
+                            <label class="form-label">RT</label>
+                            <input type="text" class="form-control" placeholder="Masukkan RT">
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="mb-1">
+                            <label class="form-label">RW</label>
+                            <input type="text" class="form-control" placeholder="Masukkan RW">
+                        </div>
+                    </div>
+                    <div class="col-sm-12">
+                        <div class="mb-1">
+                            <label class="form-label">Kelurahan</label>
+                            <input type="text" class="form-control" placeholder=" Masukan Kelurahan">
+                        </div>
+                    </div>
+                    <div class="col-sm-12">
+                        <div class="mb-1">
+                            <label class="form-label">Kecamatan</label>
+                            <input type="text" class="form-control" placeholder=" Masukan Kecamatan">
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="mb-1">
+                            <label class="form-label">PIC 1</label>
+                            <input type="text" class="form-control" placeholder="Masukkan PIC 1">
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="mb-1">
+                            <label class="form-label">Telepon</label>
+                            <input type="text" class="form-control" placeholder="Masukan Telepon">
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="mb-1">
+                            <label class="form-label">PIC 2</label>
+                            <input type="text" class="form-control" placeholder="Masukkan PIC 2">
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="mb-1">
+                            <label class="form-label">Telepon</label>
+                            <input type="text" class="form-control" placeholder="Masukan Telepon">
+                        </div>
+                    </div>
+                    <div class="col-sm-12">
+                        @guest
+                        @if (Route::has('login'))
+                        @endif
+                        @if (Route::has('register'))
+                        @endif
+                        @else
+                        <div class="mb-1">
+                            <input type="hidden" class="form-control" value="{{ Auth::user()->name }}"
+                                name="namasurveyor">
+                        </div>
+                        @endguest
+                    </div>
+                    <div class="col-sm-12">
+                        <div class="mb-1">
+                            <label class="form-label">Tanggal Disurvey</label>
+                            <input type="text" class="form-control" id="tgl">
+                        </div>
+                    </div>
+                    <div class="col-sm-12">
+                        <div class="mb-1">
+                            <label class="form-label">Foto Lokasi</label>
+                            <input type="file" class="form-control" id="foto" name="foto1">
+                        </div>
+                    </div>
+                    <div class="col-sm-12">
+                        <div class="mb-1">
+                            <label class="form-label">Foto Lokasi 2</label>
+                            <input type="file" class="form-control" id="foto" name="foto2">
+                        </div>
+                    </div>
+                    <div class="col-sm-12">
+                        <button class="btn btn-primary btn-block mt-3" type="button">Submit</button>
+                    </div>
+                </div>
+            </form>
         </div>
-      </form>
+        <div class="col-md-8">
+            <div id='map' style='width: 100%; height: 80vh;'></div>
+        </div>
     </div>
-    <div class="col-md-8">
-      <div id='map' style='width: 100%; height: 80vh;'></div>
-    </div>
-  </div>
 </div>
 
 @push('scripts')
 <script>
-  // Default Lokasi Map
-      const defaultLocation = ['110.36774955397762', '-7.824041452653281'];
-  
-      // Initialize Mapbox View
-      mapboxgl.accessToken = '{{ env('MAPBOX_ACCESS_TOKEN') }}';
-      var map = new mapboxgl.Map({
-          container: 'map',
-          center: defaultLocation,
-          zoom: 15,
-          
-      });
-  
-      // Set Map Style
-      map.setStyle('mapbox://styles/mapbox/satellite-streets-v11');
-  
-      // Add Map Controller
-      map.addControl(new mapboxgl.NavigationControl());
-  
-      // Get Latittude Longitude
-      map.on('click', function(e) {
-          const latittude = e.lngLat.lat;
-          const longtitude = e.lngLat.lng;
+    // Default Lokasi Map
+    const defaultLocation = ['110.36774955397762', '-7.824041452653281'];
 
-          document.getElementById('lat').value = latittude;
-          document.getElementById('lng').value = longtitude;
-  
-      });
-  
-      const geoJson = {
+    // Initialize Mapbox View
+    mapboxgl.accessToken = '{{ env('
+    MAPBOX_ACCESS_TOKEN ') }}';
+    var map = new mapboxgl.Map({
+        container: 'map',
+        center: defaultLocation,
+        zoom: 15,
+
+    });
+
+    // Set Map Style
+    map.setStyle('mapbox://styles/mapbox/satellite-streets-v11');
+
+    // Add Map Controller
+    map.addControl(new mapboxgl.NavigationControl());
+
+    // Get Latittude Longitude
+    map.on('click', function (e) {
+        const latittude = e.lngLat.lat;
+        const longtitude = e.lngLat.lng;
+
+        document.getElementById('lat').value = latittude;
+        document.getElementById('lng').value = longtitude;
+
+    });
+
+    const geoJson = {
         "type": "FeatureCollection",
-        "features": [
-          {
+        "features": [{
             "type": "Feature",
             "geometry": {
-              "coordinates": [
-                "110.36774955397762",
-                "-7.824041452653281"
-              ],
-              "type": "Point"
+                "coordinates": [
+                    "110.36774955397762",
+                    "-7.824041452653281"
+                ],
+                "type": "Point"
             },
             "properties": {
-              "message": "Mantap",
-              "iconSize": [
-                50,
-                50
-              ],
-              "locationId": 30,
-              "namalokasi": "Kantor Polisi Di Sini",
-              "image": "https://i0.wp.com/www.amazine.co/wp-content/uploads/2013/04/Gurita.jpg?resize=610%2C424",
-              "image2": "https://i0.wp.com/www.amazine.co/wp-content/uploads/2013/04/Gurita.jpg?resize=610%2C424",
-              "tipelokasi": "Kantor Polisi"
+                "message": "Mantap",
+                "iconSize": [
+                    50,
+                    50
+                ],
+                "locationId": 30,
+                "namalokasi": "Kantor Polisi Di Sini",
+                "image": "https://i0.wp.com/www.amazine.co/wp-content/uploads/2013/04/Gurita.jpg?resize=610%2C424",
+                "image2": "https://i0.wp.com/www.amazine.co/wp-content/uploads/2013/04/Gurita.jpg?resize=610%2C424",
+                "tipelokasi": "Kantor Polisi"
             }
-          },
-        ]
-      }
-      // Add Marker
-      const addMarkers = () => {
-          geoJson.features.forEach((location) => {
-          const {geometry, properties} = location;
-          const {message, iconSize, locationId, namalokasi, image, image2, tipelokasi} = properties;
-  
-          // Create a marker
-          var el = document.createElement('div');
-          el.className = 'marker' + locationId;
-          el.id = locationId;
-          el.style.backgroundImage = 'url(https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678111-map-marker-512.png)';
-          el.style.backgroundSize = 'cover';
-          el.style.width = '50px';
-          el.style.height = '50px';
+        }, ]
+    }
+    // Add Marker
+    const addMarkers = () => {
+        geoJson.features.forEach((location) => {
+            const {
+                geometry,
+                properties
+            } = location;
+            const {
+                message,
+                iconSize,
+                locationId,
+                namalokasi,
+                image,
+                image2,
+                tipelokasi
+            } = properties;
+
+            // Create a marker
+            var el = document.createElement('div');
+            el.className = 'marker' + locationId;
+            el.id = locationId;
+            el.style.backgroundImage =
+                'url(https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678111-map-marker-512.png)';
+            el.style.backgroundSize = 'cover';
+            el.style.width = '50px';
+            el.style.height = '50px';
 
 
-          let content = `<div style="overflow-y: auto; max-height:400px;width:100%;">
+            let content = `<div style="overflow-y: auto; max-height:400px;width:100%;">
                     <table class="table table-sm mt-2">
                          <tbody>
                             <tr>
@@ -213,29 +233,33 @@
                     </table>
                 </div>`;
 
-          let popup = new mapboxgl.Popup({ offset: 25 }).setHTML(content).setMaxWidth("400px");
+            let popup = new mapboxgl.Popup({
+                offset: 25
+            }).setHTML(content).setMaxWidth("400px");
 
-          // Add to the map
-          new mapboxgl.Marker(el)
-          .setLngLat(geometry.coordinates)
-          .setPopup(popup)
-          .addTo(map);
-          });
-      }
-  
-      // Call Add Markers
-      addMarkers();
+            // Add to the map
+            new mapboxgl.Marker(el)
+                .setLngLat(geometry.coordinates)
+                .setPopup(popup)
+                .addTo(map);
+        });
+    }
 
-      date = new Date();
-      hari = date.getDay();
-      tanggal = date.getDate();
-      bulan = date.getMonth();
-      tahun = date.getFullYear();
-      hariIndonesia = ["Senin","Selasa","Rabu","Kamis","Jumat","Sabtu","Minggu"];
-      bulanIndonesia = ["Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","November","Desember"];
-      var formattedTanggal = hariIndonesia[hari] + ', ' + tanggal + ' ' + bulanIndonesia[bulan] + ' ' + tahun;
-      document.getElementById('tgl').value = formattedTanggal;
-  
+    // Call Add Markers
+    addMarkers();
+
+    date = new Date();
+    hari = date.getDay();
+    tanggal = date.getDate();
+    bulan = date.getMonth();
+    tahun = date.getFullYear();
+    hariIndonesia = ["Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu"];
+    bulanIndonesia = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober",
+        "November", "Desember"
+    ];
+    var formattedTanggal = hariIndonesia[hari] + ', ' + tanggal + ' ' + bulanIndonesia[bulan] + ' ' + tahun;
+    document.getElementById('tgl').value = formattedTanggal;
+
 </script>
 @endpush
 @endsection
